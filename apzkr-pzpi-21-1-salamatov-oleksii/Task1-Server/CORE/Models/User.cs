dd4858416267b8f7f42c59c761fe778026c245e8
Oleksii Salamatov <165506782+NureSalamatovOleksii@users.noreply.github.com>
2024-06-15 19:16:11 +0300

    Add files via upload

---
+++ apzkr-pzpi-21-1-salamatov-oleksii/Task1-Server/CORE/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using CORE.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.Models
+{
+    public class User: BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Role { get; set; }
+        public required string Email { get; set; }
+        public bool IsEmailConfirmed { get; set; } = true;
+        public string? ConfirmationCode { get; set; }
+        public required string Password { get; set; }
+
+        public UserStatistics? UserStatistics { get; set; }
+        public ICollection<Operation> Operations { get; } = new List<Operation>();
+    }
+}
